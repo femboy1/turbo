8ac6127aa345f0ae1072da25bc5e47cfced773b4
Peter Johnson <integral.sketch1@gmail.com>
2023-02-20 21:59:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,3 +3,8 @@
 * This file is licensed under the MIT License
 * SPDX Short Identifier: MIT (https://spdx.org/licenses/MIT.html)
 */
+#include <iostream>
+int main() {
+  std::cout << "main.cpp\n";
+  return 0;
+}
